7ccc687082d41a91bdda9fb7c74630a19008681a
pppest <ppest@protonmail.com>
2020-04-29 19:36:56 -0500

    .

--- app.py
+++ app.py
@@ -3,26 +3,23 @@
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.progressbar import ProgressBar
-from kivy.properties import ObjectProperty
-from kivy.properties import StringProperty, NumericProperty
+# from kivy.uix.button import Button
+# from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 import logging
-import os
-import sys
-import time
-import math
-from PIL import Image, ImageDraw
+# import os
+# import sys
+# import time
+# import math
+# from PIL import Image, ImageDraw
 
 import config
-import lndrest
-import lntxbot
-import qr
+# import lndrest
+# import lntxbot
+# import qr
 import utils
-import importlib
+# import importlib
 
 led = "off"
 logger = logging.getLogger("MAIN")
@@ -46,6 +43,7 @@ if config.kivy:
 
     if __name__ == "__main__":
         LatmApp().run()
+
 
 elif config.papyrus:
 
